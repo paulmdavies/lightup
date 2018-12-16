78c398b3a032d1167721fd73edb127c1364db69d
Paul Davies <paulmdavies@gmail.com>
2018-12-16 11:15:47 +0000

    Naive implementation to check if square is lit

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,5 +1,52 @@
 case class Grid(squares: List[List[Square]]) {
     def isComplete(): Boolean = {
-        squares.forall(_.forall(square => square.isBlack() || square.hasLight()))
+        squares.zipWithIndex.forall {
+            case (column: List[Square], columnIndex: Int) => column.zipWithIndex.forall {
+                case (square: Square, rowIndex: Int) => square.isBlack() || isSquareLit(columnIndex, rowIndex)
+            }
+        }
+    }
+
+    def isSquareLit(columnIndex: Int, rowIndex: Int): Boolean = {
+        val squareHasLight = getSquare(columnIndex, rowIndex).hasLight()
+
+        squareHasLight || unblockedLightInLine(getColumn(columnIndex), rowIndex) || unblockedLightInLine(getRow(rowIndex), columnIndex)
+    }
+
+    private def unblockedLightInLine(line: List[Square], index: Int) = {
+        val lightIndices = line.zipWithIndex.filter(_._1.hasLight()).map(_._2)
+        val lowerLightOption = lightIndices.find(_ < index)
+
+        val lightLowerInLine = lowerLightOption match {
+            case None => false
+            case Some(lightIndex) => {
+                val intermediateSquares = line.slice(lightIndex, index)
+                intermediateSquares.nonEmpty && !intermediateSquares.exists(_.isBlack())
+            }
+        }
+
+        val higherLightOption = lightIndices.find(_ > index)
+
+        val lightHigherInLine = higherLightOption match {
+            case None => false
+            case Some(lightIndex) => {
+                val intermediateSquares = line.slice(index, lightIndex)
+                intermediateSquares.nonEmpty && !intermediateSquares.exists(_.isBlack())
+            }
+        }
+
+        lightLowerInLine || lightHigherInLine
+    }
+
+    private def getSquare(columnIndex: Int, rowIndex: Int) = {
+        getColumn(columnIndex)(rowIndex)
+    }
+
+    private def getRow(rowIndex: Int) = {
+        squares.map(_ (rowIndex))
+    }
+
+    private def getColumn(column: Int) = {
+        squares(column)
     }
 }
--- src/test/scala/GridTest.scala
+++ src/test/scala/GridTest.scala
@@ -3,18 +3,86 @@ import org.scalatest.Matchers
 
 class GridTest extends FlatSpec with Matchers {
     "A grid" should "be complete if it is empty" in {
-        Grid(List.empty).isComplete() should be(true)
+        val grid = Grid(List.empty)
+
+        grid.isComplete() should be(true)
     }
 
     it should "be complete if it contains only black squares" in {
-        Grid(List(List(Square.black()))).isComplete() should be(true)
+        val grid = Grid(List(List(Square.black())))
+
+        grid.isComplete() should be(true)
     }
 
     it should "not be complete if it contains white squares without lights" in {
-        Grid(List(List(Square.white()))).isComplete() should be(false)
+        val grid = Grid(List(List(Square.white())))
+
+        grid.isComplete() should be(false)
     }
 
     it should "be complete if it contains only white squares with lights" in {
-        Grid(List(List(Square.white(true)))).isComplete() should be(true)
+        val grid = Grid(List(List(Square.white(true))))
+
+        grid.isComplete() should be(true)
+    }
+
+    it should "be complete if all squares are lit" in {
+        val grid = Grid(List(List(Square.white(), Square.white(true), Square.white())))
+
+        grid.isComplete() should be(true)
+    }
+
+    it should "report a square as lit if it has a light" in {
+        val grid = Grid(List(List(Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(true)
+    }
+
+    it should "not report a square as lit if it does not have a light" in {
+        val grid = Grid(List(List(Square.white())))
+
+        grid.isSquareLit(0, 0) should be(false)
+    }
+
+    it should "report a square as lit if it does not have a light but a square in the same column does" in {
+        val grid = Grid(List(List(Square.white(), Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(true)
+    }
+
+    it should "report a square as lit if it does not have a light but a square in the same row does" in {
+        val grid = Grid(List(List(Square.white()), List(Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(true)
+    }
+
+    it should "not report a square as lit if it does not have a light but a square in the same column does but there is a black square between them" in {
+        val grid = Grid(List(List(Square.white(), Square.black(), Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(false)
+    }
+
+    it should "not report a square as lit if it does not have a light but a square in the same row does but there is a black square between them" in {
+        val grid = Grid(List(List(Square.white()), List(Square.black()), List(Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(false)
+    }
+
+    it should "not report a square as lit if it does not have a light but a square in a different row and column does" in {
+        val grid = Grid(List(List(Square.white(), Square.white()), List(Square.white(), Square.white(true))))
+
+        grid.isSquareLit(0, 0) should be(false)
+    }
+
+    it should "not report a square as lit if it is black without a value" in {
+        val grid = Grid(List(List(Square.black())))
+
+        grid.isSquareLit(0, 0) should be(false)
+    }
+
+    it should "not report a square as lit if it is black with a value" in {
+        val grid = Grid(List(List(Square.black(1))))
+
+        grid.isSquareLit(0, 0) should be(false)
     }
 }
