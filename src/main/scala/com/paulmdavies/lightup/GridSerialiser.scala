6fa3b78dc80384d036bff3a3a82b4709db0aad1c
Paul Davies <paulmdavies@gmail.com>
2018-12-17 21:00:32 +0000

    Add grid serialiser

--- src/main/scala/com/paulmdavies/lightup/GridSerialiser.scala
+++ src/main/scala/com/paulmdavies/lightup/GridSerialiser.scala
@@ -1,11 +1,27 @@
 package com.paulmdavies.lightup
 
 object GridSerialiser {
-    def deserialise(gridText: String) : Grid = {
-        new Grid(gridText.trim().split("\n").map(_.trim.split("").map{
+    def deserialise(gridText: String): Grid = {
+        new Grid(gridText.trim().split("\n").map(_.trim.split("").map {
             case "W" => Square.white()
             case "B" => Square.black()
             case other => Square.black(other.toInt)
         }.toList).toList)
+    }
+
+    def serialise(grid: Grid): String = {
+        grid.squares.map(_.map(square => {
+            if (square.isWhite()) {
+                square.hasLight() match {
+                    case true => "*"
+                    case false => "W"
+                }
+            } else if (square.isBlack()) {
+                square.getValue() match {
+                    case Some(value) => value.toString()
+                    case None => "B"
+                }
+            }
+        }).mkString("")).mkString("\n")
     }
 }
--- src/test/scala/com/paulmdavies/lightup/GridSerialiserTest.scala
+++ src/test/scala/com/paulmdavies/lightup/GridSerialiserTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
 class GridSerialiserTest extends FlatSpec with Matchers {
-    "A grid serializer" should "handle white squares" in {
+    "A grid deserializer" should "handle white squares" in {
         val grid = GridSerialiser.deserialise("W")
 
         grid should equal(Grid(List(List(Square.white()))))
@@ -37,5 +37,41 @@ class GridSerialiserTest extends FlatSpec with Matchers {
         )
 
         grid should equal(Grid(List(List(Square.black(1), Square.white()), List(Square.black(), Square.black(2)))))
+    }
+
+    "A grid serializer" should "handle white squares" in {
+        val grid = Grid(List(List(Square.white())))
+
+        GridSerialiser.serialise(grid) should equal("W")
+    }
+
+    it should "handle black squares" in {
+        val grid = Grid(List(List(Square.black())))
+
+        GridSerialiser.serialise(grid) should equal("B")
+    }
+
+    it should "handle numbered squares" in {
+        val grid = Grid(List(List(Square.black(4))))
+
+        GridSerialiser.serialise(grid) should equal("4")
+    }
+
+    it should "handle squares with lights" in {
+        val grid = Grid(List(List(Square.white(true))))
+
+        GridSerialiser.serialise(grid) should equal("*")
+    }
+
+    it should "handle grid in one dimension" in {
+        val grid = Grid(List(List(Square.black(1), Square.white(), Square.black(), Square.black(2))))
+
+        GridSerialiser.serialise(grid) should equal("1WB2")
+    }
+
+    it should "handle grid in two dimensions" in {
+        val grid = Grid(List(List(Square.black(1), Square.white()), List(Square.black(), Square.black(2))))
+
+        GridSerialiser.serialise(grid) should equal("1W\nB2")
     }
 }
